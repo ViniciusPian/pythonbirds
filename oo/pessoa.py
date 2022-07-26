671ab42ae1372be02ca76cc31cd2c593a0a7daf6
ViniciusPian <viniciuspian@gmail.com>
2022-07-26 05:07:03 -0300

    criado a classe Homem com herança de Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -15,9 +15,12 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos{cls.olhos} '
 
+class Homem(Pessoa):
+    pass
+
 if __name__=='__main__':
-    renzo = Pessoa(nome='Renzo')
-    luciano = Pessoa(renzo, nome='Luciano')
+    renzo = Homem(nome='Renzo')
+    luciano = Homem(renzo, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -36,3 +39,8 @@ if __name__=='__main__':
     print(luciano.olhos)
     print(Pessoa.metodo_esttico(),luciano.metodo_esttico())
     print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(renzo, Pessoa))
+    print(isinstance(renzo, Homem))
